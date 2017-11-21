3310dca93b22dbf05e3390649321ecf2efbe38ea
Unknown <robertstarr76@gmail.com>
2017-11-21 13:24:55 +1000

    Minor changes

--- connectionLogger.py
+++ connectionLogger.py
@@ -1,6 +1,5 @@
 # Created in November 2017
-# Robert Starr
-#
+
 # If your internet drops out regularly it can be handy to have evidence to show your ISP.  
 # This python script will run every 30 seconds, try and connect to Google and if it 
 # fails, it will add the date and time of the failure to a CSV file for proof.
